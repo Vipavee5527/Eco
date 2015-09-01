95098293419d64adb9ed13de1612fb8454a2e835
Chayut <chayut.cs@mail.kmutt.ac.th>
2015-09-02 00:37:37 +0700

    Bug fix

--- app/src/main/java/com/example/gigie/eco/AddFragment.java
+++ app/src/main/java/com/example/gigie/eco/AddFragment.java
@@ -12,10 +12,13 @@ import android.view.ViewGroup;
  */
 public class AddFragment extends Fragment {
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_add, container, false);
     }
+
+
 }
