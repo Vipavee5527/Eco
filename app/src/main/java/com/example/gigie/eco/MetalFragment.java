888133262c73ecb2bf2b013b516e79874f469ed6
Vipavee5527 <jijiejung_melody@hotmail.com>
2015-09-07 23:11:15 +0700

    select cat2

---
+++ app/src/main/java/com/example/gigie/eco/MetalFragment.java
@@ -0,0 +1,18 @@
+package com.example.gigie.eco;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gigie on 9/7/15 AD.
+ */
+public class MetalFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.catmetal, null, false);
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/gigie/eco/PlasticFragment.java
@@ -0,0 +1,18 @@
+package com.example.gigie.eco;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gigie on 9/7/15 AD.
+ */
+public class PlasticFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.catplastic, null, false);
+        return v;
+    }
+}
