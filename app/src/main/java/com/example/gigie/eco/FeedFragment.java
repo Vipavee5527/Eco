16c55e83cd16fad0c71217dd020a5cc563255766
Vipavee5527 <jijiejung_melody@hotmail.com>
2015-09-07 12:02:07 +0700

    newfeed

--- app/src/main/java/com/example/gigie/eco/FeedFragment.java
+++ app/src/main/java/com/example/gigie/eco/FeedFragment.java
@@ -6,16 +6,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
- * Created by gigie on 8/31/15 AD.
+ * Created by Chayut Upatham on 9/5/2015.
  */
 public class FeedFragment extends Fragment {
+
+    String[] username = {"User 1", "User 2", "User 3", "User 4", "User 5", "User 6", "User 7", "User 8"};
+    String[] comment = {"Comment 1 Comment 1 Comment 1 Comment 1 Comment 1 Comment 1 Comment 1 Comment 1 Comment 1 Comment 1",
+            "Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2",
+            "Comment 3 Comment 3 Comment 3 Comment 3 Comment 3 Comment 3",
+            "Comment 4 Comment 4 Comment 4 Comment 4 Comment 4 Comment 4",
+            "Comment 1 Comment 1 Comment 1 Comment 1 Comment 1 Comment 1 Comment 1 Comment 1 Comment 1 Comment 1",
+            "Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2",
+            "Comment 3 Comment 3 Comment 3 Comment 3 Comment 3 Comment 3",
+            "Comment 4 Comment 4 Comment 4 Comment 4 Comment 4 Comment 4"};
+    Integer[] imageId = {
+            R.drawable.com_facebook_profile_picture_blank_square,
+            R.drawable.com_facebook_profile_picture_blank_square,
+            R.drawable.com_facebook_profile_picture_blank_square,
+            R.drawable.com_facebook_profile_picture_blank_square,
+            R.drawable.com_facebook_profile_picture_blank_square,
+            R.drawable.com_facebook_profile_picture_blank_square,
+            R.drawable.com_facebook_profile_picture_blank_square,
+            R.drawable.com_facebook_profile_picture_blank_square,
+    };
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_feed, null, false);
 
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        ListView listView = (ListView) v.findViewById(R.id.list_feed);
+        NewsfeedAdapter newsfeedAdapter = new NewsfeedAdapter(getActivity(), username, comment, imageId);
+
+        listView.setAdapter(newsfeedAdapter);
+        return v;
     }
 }
---
+++ app/src/main/java/com/example/gigie/eco/NewsfeedAdapter.java
@@ -0,0 +1,45 @@
+package com.example.gigie.eco;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by gigie on 9/7/15 AD.
+ */
+public class NewsfeedAdapter extends ArrayAdapter<String> {
+
+
+    private final Activity context;
+    private final String[] username;
+    private final String[] comment;
+    private final Integer[] imageId;
+
+    public NewsfeedAdapter(Activity context, String[] username, String[] comment, Integer[] imageId) {
+        super(context, R.layout.listview, username);
+        this.context = context;
+        this.username = username;
+        this.comment = comment;
+        this.imageId = imageId;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.newsfeed_customlist, null, true);
+
+        TextView txt1 = (TextView) rowView.findViewById(R.id.text_user);
+        TextView txt2 = (TextView) rowView.findViewById(R.id.text_comment);
+
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.userPic);
+        txt1.setText(username[position]);
+        txt2.setText(comment[position]);
+
+        imageView.setImageResource(imageId[position]);
+        return rowView;
+    }
+}
