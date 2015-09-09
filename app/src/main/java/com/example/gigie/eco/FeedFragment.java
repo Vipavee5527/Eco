0f41240292ef48945ee153c66ee385ab073c0721
Vipavee5527 <jijiejung_melody@hotmail.com>
2015-09-09 15:07:00 +0700

    error feed

--- app/src/main/java/com/example/gigie/eco/FeedFragment.java
+++ app/src/main/java/com/example/gigie/eco/FeedFragment.java
@@ -3,10 +3,18 @@ package com.example.gigie.eco;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 /**
  * Created by Chayut Upatham on 9/5/2015.
@@ -22,26 +30,56 @@ public class FeedFragment extends Fragment {
             "Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2 Comment 2",
             "Comment 3 Comment 3 Comment 3 Comment 3 Comment 3 Comment 3",
             "Comment 4 Comment 4 Comment 4 Comment 4 Comment 4 Comment 4"};
-    Integer[] imageId = {
-            R.drawable.com_facebook_profile_picture_blank_square,
-            R.drawable.com_facebook_profile_picture_blank_square,
-            R.drawable.com_facebook_profile_picture_blank_square,
-            R.drawable.com_facebook_profile_picture_blank_square,
-            R.drawable.com_facebook_profile_picture_blank_square,
-            R.drawable.com_facebook_profile_picture_blank_square,
-            R.drawable.com_facebook_profile_picture_blank_square,
-            R.drawable.com_facebook_profile_picture_blank_square,
+    String[] imageId = {
+            "R.drawable.com_facebook_profile_picture_blank_square",
+            "R.drawable.com_facebook_profile_picture_blank_square",
+            "R.drawable.com_facebook_profile_picture_blank_square",
+            "R.drawable.com_facebook_profile_picture_blank_square",
+            "R.drawable.com_facebook_profile_picture_blank_square",
+            "R.drawable.com_facebook_profile_picture_blank_square",
+            "R.drawable.com_facebook_profile_picture_blank_square",
+            "R.drawable.com_facebook_profile_picture_blank_square"
     };
+
+    Integer[] pic = {0,R.drawable.cast_ic_notification_0, R.drawable.bigsofaforu};
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_feed, null, false);
+        final View v = inflater.inflate(R.layout.fragment_feed, null, false);
 
-        ListView listView = (ListView) v.findViewById(R.id.list_feed);
-        NewsfeedAdapter newsfeedAdapter = new NewsfeedAdapter(getActivity(), username, comment, imageId);
 
-        listView.setAdapter(newsfeedAdapter);
+        //        >>>SELECT by condition
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Feed");
+       // query.whereEqualTo("User","User1"); // WHERE
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> scoreList, ParseException e) {
+                if (e == null) {
+                    Log.i(">>>>>>>>>>>>>>>>>>>>>", "Retrieved " + scoreList.size() + " scores"); // Get List size
+                    username = new String[scoreList.size()+1];
+                    comment = new String[scoreList.size()+1];
+                    imageId = new String[scoreList.size()+1];
+
+                    for (ParseObject dealsObject : scoreList) {
+                        // use dealsObject.get('columnName') to access the properties of the Deals object.
+                        int i = 1;
+                        username[i]rror = (String) dealsObject.get("User");
+                        comment[i] = (String) dealsObject.get("Message");
+
+                        Log.i(">>>>>>>", comment[i]);
+
+                        i++;
+                    }
+
+                    NewsfeedAdapter newsfeedAdapter = new NewsfeedAdapter(getActivity(), username, comment, pic);
+                    ListView listView = (ListView) v.findViewById(R.id.list_feed);
+                    listView.setAdapter(newsfeedAdapter);
+                } else {
+                    Log.i("score", "Error: " + e.getMessage());
+                }
+            }
+        });
+
         return v;
     }
 }
